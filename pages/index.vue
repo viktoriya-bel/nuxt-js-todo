4f316c75604f6c195ea5870a2c4878ec44460467
Виктория <vita152010@mail.ru>
2023-08-22 18:02:27 +0400

    getting todo list && add module vuetify

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-table density="compact">
+        <thead>
+        <tr>
+            <th class="text-left">Id</th>
+            <th class="text-left">Todo</th>
+            <th class="text-left">Completed</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+                v-for="item in todos"
+                :key="item.id"
+                :class="{ completed: item.completed }"
+        >
+            <td class="text-transform">{{ item.id }}</td>
+            <td class="text-transform">{{ item.todo }}</td>
+            <td class="text-xs-center">
+                <v-checkbox :model-value="item.completed"
+                ></v-checkbox>
+            </td>
+        </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<script setup>
+    const { data } = await useFetch('https://dummyjson.com/todos');
+    const todos = [...data._rawValue.todos];
+</script>
+
+<style scoped>
+    tr.completed{
+        color: blue;
+    }
+
+    tr.completed .text-transform{
+        text-decoration-line: line-through;
+    }
+</style>
